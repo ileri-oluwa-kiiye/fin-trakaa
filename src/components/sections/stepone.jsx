01df2e84c2f585a923e00853b9ae22e60d7d2ccf
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-03-10 02:32:39 +0100

    added the privacy policy

--- src/components/sections/stepone.jsx
+++ src/components/sections/stepone.jsx
@@ -47,7 +47,7 @@ const Stepone = () => {
         </div>
         <div>
             <input type="radio" name="agree" id="agree" />
-            <label htmlFor="agree">I agree to all the <a href="https://www.privacypolicies.com/live/228b4da0-e337-48b7-b9e8-02a7d2d1347a" target="_blank" style={{color:'inherit', textDecoration: 'none'}}><span>terms and privacy policy</span></a> of Fin-traka</label> 
+            <label htmlFor="agree">I agree to all the <a href="https://www.privacypolicies.com/live/228b4da0-e337-48b7-b9e8-02a7d2d1347a" rel="noreferrer" target="_blank" style={{color:'inherit', textDecoration: 'none'}}><span>terms and privacy policy</span></a> of Fin-traka</label> 
         </div>
         
         <Link to="/confirmation" style={{color:'inherit', textDecoration: 'none'}}><div>
